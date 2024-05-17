32aeec6a71b8d28d3819da2db8a92fddb238fc85
JuanDiegoWS <mejiamaestrejuandiego@gmail.com>
2024-05-16 19:36:19 -0500

    Creacion de la base de datos

---
+++ database/migrations/2024_05_17_002513_create_course_sales_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Course;
+use App\Models\CourseSaleStatus;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('course_sales', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class)->constrained();
+            $table->foreignIdFor(CourseSaleStatus::class)->constrained();
+            $table->json('courses');
+            $table->json('records');
+            $table->decimal('total_to_pay');
+            $table->datetime('purchase_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('course_sales');
+    }
+};
