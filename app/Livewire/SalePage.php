9279e11d3ae4336d3df3a5c811627050620dd9d4
JuanDiegoWS <mejiamaestrejuandiego@gmail.com>
2024-05-18 22:59:43 -0500

    agregar  paginacion

--- app/Livewire/SalePage.php
+++ app/Livewire/SalePage.php
@@ -3,14 +3,25 @@
 namespace App\Livewire;
 
 use App\Models\CourseSale;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 use Livewire\Component;
 
 class SalePage extends Component
 {
+
+    public $countRow = 10;
+    
     public function render()
     {
         return view('livewire.sale-page', [
-            'sales' => CourseSale::with('user')->get()
+            'sales' => $this->getData()
         ]);
+    }
+
+    public function getData(): LengthAwarePaginator
+    {
+        $query =  CourseSale::with('user');
+        $query->where('id', '!=', auth()->user()->id);
+        return $query->paginate($this->countRow ?? 10);
     }
 }
--- resources/views/livewire/sale-page.blade.php
+++ resources/views/livewire/sale-page.blade.php
@@ -1,5 +1,22 @@
-<div class="max-w-[80vw] mx-auto">
+<div class="max-w-[80vw] mx-auto my-4">
     <h1 class="text-3xl font-bold mb-6">Ventas Realizadas</h1>
+
+    <div class="w-[80vw]   mx-auto  flex justify-between mb-4">
+
+        <select id="countries" class="w-60 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5" wire:model.live="countRow">
+
+            <option value="10">10 Registros</option>
+            <option value="25">25 Registros</option>
+            <option value="50">50 Registros</option>
+            <option value="100">100 Registros</option>
+        </select>
+      
+    </div>
+
+    <div class="w-[80vw] mx-auto my-4">
+        {{ $sales->links() }}
+    </div>
+
     <table class="min-w-full bg-white">
         <thead>
             <tr>
@@ -12,7 +29,7 @@
         </thead>
         <tbody>
             @foreach ($sales as $sale)
-                <tr>
+                <tr class="text-center align-middle items-center ">
                     <td class="py-2 px-4 border-b">{{ $sale->id }}</td>
                     <td class="py-2 px-4 border-b">{{ $sale->user->name }}</td>
                     <td class="py-2 px-4 border-b">
@@ -28,4 +45,9 @@
             @endforeach
         </tbody>
     </table>
+
+    <div class="w-[80vw] mx-auto my-4">
+        {{ $sales->links() }}
+    </div>
+
 </div>
